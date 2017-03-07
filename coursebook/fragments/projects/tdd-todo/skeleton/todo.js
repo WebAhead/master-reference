172ff4be3586a5540d8d130bdbde83b39794049a
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-07 15:55:01 +0000

    final tweeks and bug fixes for todo project
    related #184

--- coursebook/fragments/projects/tdd-todo/skeleton/todo.js
+++ coursebook/fragments/projects/tdd-todo/skeleton/todo.js
@@ -42,7 +42,7 @@ var todoFunctions = {
 // it keeps everything inside hidden from the rest of our application
 (function() {
   // This is the dom node where we will keep our todo
-  var container = document.getElementById('todo-container'),
+  var container = document.getElementById('todo-container');
   var addTodoForm = document.getElementById('add-todo');
 
   var state = [
@@ -91,7 +91,7 @@ var todoFunctions = {
 
 
   // you should not need to change this function
-  var update = function(state) {
+  var update = function(newState) {
     state = newState;
     renderState(state);
   }
@@ -101,12 +101,12 @@ var todoFunctions = {
     var todoListNode = document.createElement('ul');
 
     state.forEach(function(todo) {
-      todoListNode.appendChild(controller.createTodoNode(todo))
+      todoListNode.appendChild(createTodoNode(todo))
     });
 
     // you may want to add a class for css
     container.replaceChild(todoListNode, container.firstChild);
   }
 
-  if (controller.container) controller.render(state);
+  if (container) render(state);
 }())
