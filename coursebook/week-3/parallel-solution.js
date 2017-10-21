7726b6bb178169a39f9d8f2ea6bd5b3a199d501c
Matt Davis <matthewpeterdavis@gmail.com>
2017-10-21 11:37:56 +0300

    fix indentation errors :"

--- coursebook/week-3/parallel-solution.js
+++ coursebook/week-3/parallel-solution.js
@@ -1,4 +1,4 @@
-function parallel (tasks, callback) {
+function parallel(tasks, callback) {
   // create array to hold results
   var resultsArr = [];
   // counter to count the loop as resultsArr.length will be misleading
@@ -7,52 +7,52 @@ function parallel (tasks, callback) {
   var hasFailed = false
   //loop through each task
   tasks.forEach((task, i) => {
-      task((err, result) => {
-        //check if we've failed yet, if so, do not run
-        if (!hasFailed) {
-          // if err call the callback
-          if (err) {
-            hasFailed = true
-            callback(err)
-            return
-          }
-          // if no error add to the resultsArr and increase the counter
-          resultsArr[i] = result
-          counter++
-          // when all the tasks are done, call the callback with arr
-          if (counter === tasks.length){
-            callback(null, resultsArr)
-          }
+    task((err, result) => {
+      //check if we've failed yet, if so, do not run
+      if (!hasFailed) {
+        // if err call the callback
+        if (err) {
+          hasFailed = true
+          callback(err)
+          return
         }
-      })
+        // if no error add to the resultsArr and increase the counter
+        resultsArr[i] = result
+        counter++
+        // when all the tasks are done, call the callback with arr
+        if (counter === tasks.length) {
+          callback(null, resultsArr)
+        }
+      }
     })
+  })
 }
 
 parallel([
   function(callback) {
     setTimeout(function() {
-      callback(null,1);
-    },2000);
+      callback(null, 1);
+    }, 2000);
   },
   function(callback) {
     setTimeout(function() {
-      callback(null,2);
-    },1000);
+      callback(null, 2);
+    }, 1000);
   },
   function(callback) {
     setTimeout(function() {
-      callback(null,3);
-    },1500);
+      callback(null, 3);
+    }, 1500);
   },
   // function(callback) {
   //   setTimeout(function() {
   //     callback('boom');
   //   },1200);
   // }
-], function(err,result) {
+], function(err, result) {
   if (err) {
-    console.log('err: ',err); // boom
+    console.log('err: ', err); // boom
   } else {
-    console.log('result: ',result); // [1,2,3]
+    console.log('result: ', result); // [1,2,3]
   }
 });
